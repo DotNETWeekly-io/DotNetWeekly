7f97a5f52ceeade0464f4a31340ec52b87e3f24e
gaufung <gaufung@outlook.com>
2021-12-06 22:50:45 +0800

    setup website

---
+++ Website/src/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-heaer">
+                    <p>Welcome to .NET Weekly.</p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
