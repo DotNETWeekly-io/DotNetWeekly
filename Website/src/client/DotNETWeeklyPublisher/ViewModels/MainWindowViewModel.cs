323c08cdad3dba493a438a2c8b886ffeff3f7669
gaufung <gaufung@outlook.com>
2022-01-04 21:01:50 +0800

    add publisher client

---
+++ Website/src/client/DotNETWeeklyPublisher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNETWeeklyPublisher.ViewModels
+{
+    public class MainWindowViewModel : NotificationObject
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; this.RaisePropertyChanged("Name"); }
+        }
+
+        private readonly IConfiguration _configuration;
+
+        public MainWindowViewModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Name = configuration.GetSection("ConnectionStrings")["DefaultConnection"];
+        }
+
+    }
+}
---
+++ Website/src/client/DotNETWeeklyPublisher/ViewModels/NotificationObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DotNETWeeklyPublisher.ViewModels
+{
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
