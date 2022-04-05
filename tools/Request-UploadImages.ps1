940fc446799d85558fba3799878347bb49e19dd5
gaufung <gaufung@outlook.com>
2022-04-05 22:42:44 +0800

    update the pic bed

---
+++ tools/Request-UploadImages.ps1
@@ -0,0 +1,77 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]
+    $ResourceGroupName = 'dotnetweekly',
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $StorageAccountName = 'dotnetweeklypics',
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $EpisodeName,
+
+    # Parameter help description
+    [Parameter(Mandatory=$true)]
+    [string[]]
+    $ImageFilePaths
+)
+
+function Get-ImageMimeMapping {
+    param (
+        # Parameter help description
+        [Parameter(Mandatory=$true)]
+        [string]
+        $fileName
+    )
+    $extension = [System.IO.Path]::GetExtension($fileName);
+    $mimeTypeMap = @{
+        '.png'                          = 'image/png'
+        '.jpe'                          = 'image/jpeg'
+        '.jpeg'                         = 'image/jpeg'
+        '.jpg'                          = 'image/jpeg'
+        '.gif'                          = 'image/gif'
+        '.bmp'                          = 'image/bmp'
+    }
+
+    return $mimeTypeMap[$extension];
+}
+
+$accessToken = Get-AzAccessToken;
+if (-not $accessToken -or $accessToken.ExpiresOn.UtcDateTime -gt [System.DateTime]::UtcNow) {
+    Connect-AzAccount
+}
+
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
+
+if (-not $storageAccount) {
+    Write-Error "Storage Account $($StorageAccountName) doesn't exist in the $($ResourceGroupName) resource group";
+    return
+}
+
+$context = $storageAccount.Context 
+
+$container = Get-AzStorageContainer -Context $context -Container $EpisodeName
+
+if (-not $container) {
+    New-AzStorageContainer -Name $EpisodeName -Permission Container -Context $context
+}
+
+$container = Get-AzStorageContainer -Context $context -Name $EpisodeName
+
+$ImageFilePaths | Foreach-Object {
+    $fileName = [System.IO.Path]::GetFileName($_);
+    $contentType = Get-ImageMimeMapping $fileName
+    $blob2HT = @{
+        File             = $_
+        Container        = $EpisodeName
+        Blob             = $fileName
+        Context          = $context
+        StandardBlobTier = 'Cool'
+        Properties         = @{"ContentType" = $contentType }
+    }
+    $blob = Set-AzStorageBlobContent @blob2HT
+    $uri = $blob.ICloudBlob.Uri
+    Write-Host "$($_) -> $($uri)"
+}
