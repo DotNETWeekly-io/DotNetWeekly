398dd8c5ca7305c04eb761151beaf0a8e68fb503
gaufung <gaufung@outlook.com>
2022-05-24 20:34:41 +0800

    add spinner

--- Website/src/frontend/src/EpisodeList.tsx
+++ Website/src/frontend/src/EpisodeList.tsx
@@ -11,6 +11,8 @@ import {
     ImageFit,
     DocumentCardDetails,
     DocumentCardTitle,
+    Spinner,
+    SpinnerSize,
 } from '@fluentui/react';
 import md, { Image } from 'markdown-ast';
 
@@ -31,6 +33,7 @@ const cardStyles: IDocumentCardStyles = {
 
 export interface EpisodeListState {
     episodes: Episode[][];
+    loading: boolean;
 }
 
 export class EpisodeList extends React.Component<any, EpisodeListState> {
@@ -41,6 +44,7 @@ export class EpisodeList extends React.Component<any, EpisodeListState> {
         super(any);
         this.state = {
             episodes: [],
+            loading: true,
         };
     }
 
@@ -54,7 +58,7 @@ export class EpisodeList extends React.Component<any, EpisodeListState> {
                 }
                 return -1;
             });
-            for (let i = 0; i < episodes.length; ) {
+            for (let i = 0; i < episodes.length;) {
                 const row: Episode[] = [];
                 for (let j = 0; j < column && i < episodes.length; j++) {
                     row.push(episodes[i]);
@@ -64,8 +68,9 @@ export class EpisodeList extends React.Component<any, EpisodeListState> {
             }
             this.setState({
                 episodes: episodesGroup,
+                loading: false,
             });
-        } catch (error) {}
+        } catch (error) { }
     }
 
     retrieveImgeSource(episode: Episode): string {
@@ -80,53 +85,67 @@ export class EpisodeList extends React.Component<any, EpisodeListState> {
 
     render(): JSX.Element {
         return (
-            <Stack
-                styles={{
-                    root: {
-                        height: '100px',
-                    },
-                }}
-            >
+            <>
                 {
-                    // eslint-disable-next-line array-callback-return
-                    this.state.episodes.map((episodes) => (
-                        <>
-                            <Separator />
-                            <Stack
-                                horizontal
-                                horizontalAlign="center"
-                                tokens={stackTokens}
-                            >
-                                {
-                                    // eslint-disable-next-line array-callback-return
-                                    episodes.map((episode) => (
-                                        <DocumentCard
-                                            styles={cardStyles}
-                                            onClickHref={`/episode/${episode.id}`}
-                                        >
-                                            <DocumentCardImage
-                                                height={120}
-                                                imageFit={
-                                                    ImageFit.centerContain
-                                                }
-                                                imageSrc={this.retrieveImgeSource(
-                                                    episode,
-                                                )}
-                                            />
-                                            <DocumentCardDetails>
-                                                <DocumentCardTitle
-                                                    title={episode.title}
-                                                    shouldTruncate
-                                                ></DocumentCardTitle>
-                                            </DocumentCardDetails>
-                                        </DocumentCard>
-                                    ))
-                                }
-                            </Stack>
-                        </>
-                    ))
+                    this.state.loading ? (<Stack verticalFill verticalAlign="center" horizontalAlign="center" styles={{
+                        root: {
+                            position: "fixed",
+                            top: "50%",
+                            left: "50%",
+                            transform: 'translate(-50%,-50%)',
+                        }
+                    }}>
+                        <Spinner label="Loading" labelPosition="bottom" size={SpinnerSize.large}>
+                        </Spinner>
+                    </Stack>) : (<Stack
+                        styles={{
+                            root: {
+                                height: '100px',
+                            },
+                        }}
+                    >
+                        {
+                            // eslint-disable-next-line array-callback-return
+                            this.state.episodes.map((episodes) => (
+                                <>
+                                    <Separator />
+                                    <Stack
+                                        horizontal
+                                        horizontalAlign="center"
+                                        tokens={stackTokens}
+                                    >
+                                        {
+                                            // eslint-disable-next-line array-callback-return
+                                            episodes.map((episode) => (
+                                                <DocumentCard
+                                                    styles={cardStyles}
+                                                    onClickHref={`/episode/${episode.id}`}
+                                                >
+                                                    <DocumentCardImage
+                                                        height={120}
+                                                        imageFit={
+                                                            ImageFit.centerContain
+                                                        }
+                                                        imageSrc={this.retrieveImgeSource(
+                                                            episode,
+                                                        )}
+                                                    />
+                                                    <DocumentCardDetails>
+                                                        <DocumentCardTitle
+                                                            title={episode.title}
+                                                            shouldTruncate
+                                                        ></DocumentCardTitle>
+                                                    </DocumentCardDetails>
+                                                </DocumentCard>
+                                            ))
+                                        }
+                                    </Stack>
+                                </>
+                            ))
+                        }
+                    </Stack>)
                 }
-            </Stack>
+            </>
         );
     }
 }
--- Website/src/frontend/src/EpisodePage.tsx
+++ Website/src/frontend/src/EpisodePage.tsx
@@ -4,18 +4,24 @@ import { useParams } from 'react-router';
 import ReactMarkdown from 'react-markdown';
 import styles from './EpisodePage.module.css';
 import './EpisodePage.module.css';
-import { Stack } from '@fluentui/react';
+import {
+    Stack,
+    Spinner,
+    SpinnerSize,
+} from '@fluentui/react';
 import Header from './Header';
 import Footer from './Footer';
 
 export const EpisodePage = () => {
     const [episode, setEpisode] = React.useState<Episode | null>(null);
+    const [loading, setLoading] = React.useState<boolean>(true);
     const { episodeId } = useParams();
 
     React.useEffect(() => {
         const doGetEpisode = async (episodeId: number) => {
             const foundEpisode = await getEpisodeById(episodeId);
             setEpisode(foundEpisode);
+            setLoading(false);
         };
         if (episodeId) {
             doGetEpisode(Number(episodeId));
@@ -25,9 +31,21 @@ export const EpisodePage = () => {
     return (
         <Stack verticalFill>
             <Header></Header>
-            <div className={styles.episodePage}>
-                <ReactMarkdown>{episode ? episode.content : ''}</ReactMarkdown>
-            </div>
+            {
+                loading ? (<Stack verticalFill verticalAlign="center" horizontalAlign="center" styles={{
+                    root: {
+                        position: "fixed",
+                        top: "50%",
+                        left: "50%",
+                        transform: 'translate(-50%,-50%)',
+                    }
+                }}>
+                    <Spinner label="Loading" labelPosition="bottom" size={SpinnerSize.large}>
+                    </Spinner>
+                </Stack>) : (<div className={styles.episodePage}>
+                    <ReactMarkdown>{episode ? episode.content : ''}</ReactMarkdown>
+                </div>)
+            }
             <Footer></Footer>
         </Stack>
     );
