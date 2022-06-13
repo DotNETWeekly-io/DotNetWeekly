246d7c6b6bb607f5b3ae00e6b41dab5ce6225fca
gaufung <gaufung@outlook.com>
2022-06-13 22:08:27 +0800

    cancellation

---
+++ Samples/DotNetWeeklySample/CancellationConsoleSample/Program.cs
@@ -0,0 +1,23 @@
+﻿CancellationTokenSource soruce = new CancellationTokenSource();
+Task.Run(() =>
+{
+    Console.ReadKey();
+    soruce.Cancel();
+});
+CancellationToken token = soruce.Token;
+try
+{
+    while (true)
+    {
+        token.ThrowIfCancellationRequested();
+        Console.WriteLine("Cancel operation in 10 seconds");
+        await Task.Delay(10000, token);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"{e.GetType()}: {e.ToString()}");
+}
+
+
+soruce.Dispose();
