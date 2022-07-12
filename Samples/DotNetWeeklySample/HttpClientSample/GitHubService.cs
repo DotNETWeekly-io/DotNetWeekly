76122cca557efee2e0d1010d4dd832418ab7b9dd
gaufung <gaufung@outlook.com>
2022-07-12 21:09:06 +0800

    add sample

---
+++ Samples/DotNetWeeklySample/HttpClientSample/GitHubService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    public class GitHubService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public GitHubService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory=httpClientFactory;   
+        }
+
+        public System.Net.HttpStatusCode Fetch()
+        {
+            string url = "https://api.github.com/repos/DotNETWeekly-io/DotNetWeekly/contents/doc";
+            var httpClient = _httpClientFactory.CreateClient();
+            var result = httpClient.GetAsync(url).Result;
+            return result.StatusCode;
+        }
+    }
+}
---
+++ Samples/DotNetWeeklySample/HttpClientSampleTest/Usings.cs
@@ -0,0 +1,8 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+global using Moq;
+
+global using RichardSzalay.MockHttp;
+global using RichardSzalay.MockHttp.Matchers;
+
+global using HttpClientSample;
