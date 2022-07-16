483bb445b75faac7523016364c505c705ae24a0a
gaufung <gaufung@outlook.com>
2022-07-16 14:48:38 +0800

    index sample

---
+++ Samples/DotNetWeeklySample/IndexSample/NameListSample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexSample
+{
+    public class NameListSample
+    {
+        public static string GetIndex(NameList list)
+        {
+            return list[^1];
+        }
+
+        public static List<string> GetRange(NameList list)
+        {
+            return list[^1..];
+        }
+    }
+}
